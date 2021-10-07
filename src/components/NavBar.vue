4ca9ca7b0e3b929a5b225fcb1eab5e53efdcb72e
Massiel Escano <massielescano@yahoo.com>
2021-10-07 16:48:37 -0400

    nav bar displayed, buttons linked to respected profiles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,27 @@
- <template>
-     <h1>This is navbar</h1>
- </template>
+<template>
+  <mdb-navbar color="grey" class=" lighten-4 py-4">
+    <mdb-navbar-nav nav center>
+      <mdb-nav-item>
+        <mdb-btn href = "https://www.linkedin.com/in/massiel-escano/" tag="a" size="lg" floating class="btn-li" icon="linkedin-in" fab></mdb-btn>
+      </mdb-nav-item>
+      <mdb-nav-item>
+        <mdb-btn href= "https://github.com/Massiel50" tag="a" size="lg" floating class="btn-git" icon="github" fab></mdb-btn>
+      </mdb-nav-item>
+    </mdb-navbar-nav>
+  </mdb-navbar>
+</template>
 
 
 <script>
-    export default {
-        name: 'NavBar',
-        
+  import { mdbNavbar, mdbNavbarNav, mdbBtn} from 'mdbvue';
+  export default {
+    name: 'NavBar',
+    components: {
+      mdbNavbar,
+      mdbNavbarNav,
+      mdbBtn
     }
+  }
 </script>
  
  
