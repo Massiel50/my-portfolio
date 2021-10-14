e70e23371c9b522482930e8a3098d82a0bff355d
Massiel Escano <massielescano@yahoo.com>
2021-10-14 10:45:13 -0400

    created card for skills section of page

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,23 @@
- <template>
-     <h1>This is skills</h1>
- </template>
-
+<template>
+  <mdb-card class="card-body" >
+    <mdb-card-title>
+      <h2>SKILLS</h2>
+      </mdb-card-title>
+    <mdb-card-text></mdb-card-text>
+    <div class="flex-row">
+      <p>boxes of pics of projects as a link</p>
+    </div>
+  </mdb-card>
+</template>
 
 <script>
-    export default {
-        name: 'Skills',
-        
+  import { mdbCard, mdbCardTitle, mdbCardText } from 'mdbvue';
+  export default {
+    name: 'About',
+    components: {
+      mdbCard,
+      mdbCardTitle,
+      mdbCardText
     }
+  }
 </script>
