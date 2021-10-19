d5cdf0fcac3c6f0773cd5ee8b4411ac2a4386360
Massiel Escano <massielescano@yahoo.com>
2021-10-19 11:42:55 -0400

    thumbnails placeholder in projects sections

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,7 +5,27 @@
       </mdb-card-title>
     <mdb-card-text></mdb-card-text>
     <div class="flex-row">
-      <p>boxes of pics of projects as a link</p>
+      <div id= "row1">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+        style="width: 200px">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+         style="width: 200px">
+      </div>
+      
+      <div id= "row2">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+        style="width: 200px">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+         style="width: 200px">
+      </div>
+
+      <div id= "row3">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+        style="width: 200px">
+        <img src="https://mdbootstrap.com/img/Others/documentation/img%20(75)-mini.jpg" alt="thumbnail" class="hover"
+         style="width: 200px">
+      </div>
+
     </div>
   </mdb-card>
 </template>
